04d1a3f95e8b7fcd47e9280aa991875eee4ad698
Het0c <hlopez.htilg@gmail.com>
2023-11-14 12:31:39 -0300

    Hola mundo xd

---
+++ src/pjtcasinoduoc/PjtCasinoDuoc.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pjtcasinoduoc;
+
+/**
+ *
+ * @author hecto
+ */
+public class PjtCasinoDuoc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("hola mundo xd");
+    }
+    
+}
